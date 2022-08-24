8f6ffd54115acc0d4bcd80acb1d9c55e34927e35
CrimsonCoder42 <nostro37@gmail.com>
2022-08-24 11:14:01 -0400

    Inheritance and traits

--- src/main/scala/lectures/oop/InheritanceAndTraits.scala
+++ src/main/scala/lectures/oop/InheritanceAndTraits.scala
@@ -1,5 +1,60 @@
 package lectures.oop
 
-object InheritanceAndTraits {
+object InheritanceAndTraits extends App {
+
+  // single class inheritance
+
+  class Animal {
+    val creatureType = "Wild"
+
+    def eat = println("nomnom")
+  }
+
+  class Cat extends Animal {
+    def crunch = {
+      eat
+      println("crunch crunch")
+    }
+  }
+
+  val cat = new Cat
+  cat.crunch
+
+  // constructors
+  class Person(name: String, age: Int) {
+    def this(name: String) = this(name,0)
+  }
+  class Adult(name: String, age: Int, idCard: String) extends Person(name, age)
+
+  // overriding
+
+  class Dog(override val creatureType: String) extends Animal {
+    //override val creatureType = "domestic"
+    override def eat = {
+      super.eat
+      println("crunch, crunch")
+    }
+  }
+
+  val dog = new Dog("K9")
+  dog.eat
+  println(dog.creatureType)
+
+  // type substitution (broad: polymorphism)
+  val unknownAnimal: Animal = new Dog("K9")
+  unknownAnimal.eat
+
+  // overriding vs overLOADING
+
+
+  // super
+
+
+
+  // preventing overrides
+  // 1 - final on member
+  // 2 - final on class
+
+
 
 }
